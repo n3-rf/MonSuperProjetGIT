6e5f1971a16c00e2f0e9906b9496bf641df71b86
Théo Regueira <theoregu@gmail.com>
2024-11-19 18:51:56 +0100

    Mise à jour du back-end pour afficher les nouvelles informations

--- back/traitement.php
+++ back/traitement.php
@@ -1,7 +1,10 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $nom = $_POST["nom"];
+    $prenom = $_POST["prenom"]; // Ajout du prénom
     $email = $_POST["email"];
-    echo "Merci $nom, votre email est $email";
+    $age = $_POST["age"]; // Ajout de l'age
+
+    echo "<p>Merci $prenom $nom, votre email est $email et vous avez $age ans.</p>";
 }
 ?>
